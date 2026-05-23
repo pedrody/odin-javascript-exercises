b7713f253e4b24b8dba2396b039a137a9c499540
Pedro Azevedo <91354573+pedrody@users.noreply.github.com>
2026-05-23 15:26:11 -0300

    Complete 09_sumAll

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
+    if (typeof(min) != 'number' || 
+        typeof(max) != 'number' ||
+        min < 0 || 
+        max < 0 ||
+        !Number.isInteger(min) ||
+        !Number.isInteger(max)) {
+            return 'ERROR';
+    }
+    
+    if (max < min) {
+        [max, min] = [min, max];
+    } 
 
+    let sum = 0;
+    for (let i = min; i <= max; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
