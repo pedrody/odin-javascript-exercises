855e7b5ee48bbc4a1bdefc1d3d5704946d1f00dc
Pedro Azevedo <91354573+pedrody@users.noreply.github.com>
2026-06-11 14:46:25 -0300

    Complete 16_findTheOldest

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    people.sort((a, b) => {
+        a.yearOfDeath = a.yearOfDeath === undefined ? new Date().getFullYear() : a.yearOfDeath;
+        b.yearOfDeath = b.yearOfDeath === undefined ? new Date().getFullYear() : b.yearOfDeath;
 
+        return (b.yearOfDeath - b.yearOfBirth) - (a.yearOfDeath - a.yearOfBirth);
+    })
+        
+    return people[0];
 };
 
 // Do not edit below this line
