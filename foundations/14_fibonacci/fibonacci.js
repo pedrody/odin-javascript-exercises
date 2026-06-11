dccda5224fb080b05a92095f26682a8e528c14fc
Pedro Azevedo <91354573+pedrody@users.noreply.github.com>
2026-06-11 13:53:09 -0300

    Complete 14_fibonacci

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(position) {
+    if (position < 0) {
+        return "OOPS";
+    }
 
+    let previous = 0;
+    let current = 1;
+    let next = current + previous;
+
+    for (let i = 0; i < position; i++) {
+        previous = current;
+        current = next;
+        next = current + previous;
+    }
+
+    return previous;
 };
 
 // Do not edit below this line
